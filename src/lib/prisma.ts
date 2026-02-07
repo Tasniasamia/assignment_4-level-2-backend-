a55f138b3239211cbe0e25368eb1f3f1a06960e5
tasniaarhamarham <tasniaarhamarham@gmail.com>
2026-02-08 00:12:23 +0600

    some of changes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,16 @@
+// import "dotenv/config";
+// import { PrismaPg } from '@prisma/adapter-pg'
+// import { PrismaClient } from "../../generated/prisma/client";
+
+// const connectionString = `${process.env.DATABASE_URL}`
+
+// const adapter = new PrismaPg({ connectionString })
+// const prisma = new PrismaClient({ adapter })
+
+// export { prisma }
+
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from "../../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`
-
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
-
-export { prisma }
+// Single instance of PrismaClient for the whole project
+export const prisma = new PrismaClient();
